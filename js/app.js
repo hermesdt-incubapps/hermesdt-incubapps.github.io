331734f59678ff0404745dece10e2519e8b2cadf
Álvaro Durán Tovar <hermesdt@gmail.com>
2014-11-30 23:03:31 +0100

    Added jquery scrollto

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function($){
+  $("[data-goto]").on("click", function(evt){
+    evt.preventDefault();
+
+    var gotoId = $(this).attr("data-goto");
+    var destination = $("#" + gotoId);
+    $.scrollTo(destination, 600);
+    return false;
+  });
+});
